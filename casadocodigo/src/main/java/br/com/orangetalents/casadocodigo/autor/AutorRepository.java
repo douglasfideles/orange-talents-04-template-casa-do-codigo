ba8dcf6ee546caa0d20002326183893224df254e
Douglas Rodrigues Fideles <47896736+douglasfideles@users.noreply.github.com>
2021-06-23 22:54:49 -0300

    deleted

--- casadocodigo/src/main/java/br/com/orangetalents/casadocodigo/autor/AutorRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.orangetalents.casadocodigo.autor;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface AutorRepository extends CrudRepository<Autor, Long>{
-
-	
-}
